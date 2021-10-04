c51bdf411ad424dc5a888c0e5e9b9c180582b59a
XinyuC <xinyuc96@gmail.com>
2021-10-04 00:20:25 -0400

    Update script_videoProcessing.m

--- Lane Detection/Functions/script_videoProcessing.m
+++ Lane Detection/Functions/script_videoProcessing.m
@@ -1,3 +1,12 @@
+%{
+This script is used to show the result of a video test. For the test video,
+the function works good most of the time, but when the detection area is
+covered by a shadow, the function is hardly to detect the lane marker.
+Different contrast enhancement techniques were tested, but it seems like
+they only add more noises to the video/image. The next step is solve the
+'shadow' problem.
+%}
+
 %% Prepare the script
 close all
 clear all
@@ -11,7 +20,7 @@ addpath(folder_name)
 vidObj = VideoReader('cherryspring.mp4')
 frame_num = vidObj.NumFrames;
 duration = floor(vidObj.Duration);
-pause_time = 1/vidObj.FrameRate;
+% pause_time = 1/vidObj.FrameRate;
 pause_time = 0.05;
 
 %%
@@ -26,17 +35,11 @@ while vidObj.CurrentTime < duration
     image_hsv = fcn_LaneDet_dataPreparation(vidFrame_rgb);
 % Call fcn_LaneDet_removeNoise
     clean_image_hsv = fcn_LaneDet_removeNoise(image_hsv);
-    figure(1)
-    fig_h = fcn_LaneDet_view_density(clean_image_hsv);
-%     yellow_mask = fcn_LaneDet_yellowThresholding(vidFrame_rgb); % Call fcn_LaneDet_yellowThresholding
+
+    yellow_mask = fcn_LaneDet_yellowThresholding(vidFrame_rgb); % Call fcn_LaneDet_yellowThresholding
 % Show the mask
-%     figure(1)
-%     imshowpair(vidFrame_rgb, yellow_mask, 'montage')
-%     imshowpair(vidFrame_rgb, fig_h, 'montage')
-%     figure(2)
-%     imshow(vidFrame_rgb)
-
-
+    figure(1)
+    imshowpair(vidFrame_rgb, yellow_mask, 'montage')
 end
 toc
 fprintf('end')
